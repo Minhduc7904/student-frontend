582905fbe5666743e4e7f48e9e0fede15be66cd5
Minhduc7904 <nmduc7904@gmail.com>
2026-02-25 16:58:56 +0700

    feat: Simplify Nginx SPA routing and remove unnecessary redirects

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/student/', // 👈 thêm dòng này
-
   plugins: [
     react(),
-    {
-      name: 'trailing-slash-redirect',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url === '/student') {
-            res.writeHead(301, { Location: '/student/' });
-            res.end();
-            return;
-          }
-          next();
-        });
-      },
-    },
   ],
 
   server: {
-    port: 3000,       // 👈 đổi port
+    port: 3000,
   },
 
   resolve: {
